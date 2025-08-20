11fbcc643cdbd21735a1b3daeb02fe48386ab1b8
surajgdesai7600-dot <surajgdesai7600@gmail.com>
2025-08-20 09:52:46 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Suraj's Funnel Analysis Portfolio</h1>
+      <p>Subscription SaaS Funnel Analysis Dashboard</p>
+    </div>
+  );
+}
+
+export default App;
